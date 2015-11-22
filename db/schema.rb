aed31a6d333be471cc92455bcc811e3f53c64229
paras <rockypro92@gmail.com>
2015-11-22 16:02:13 +0530

    Added extra user attributes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151121063408) do
+ActiveRecord::Schema.define(version: 20151122093453) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -27,6 +27,9 @@ ActiveRecord::Schema.define(version: 20151121063408) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.text     "bio"
+    t.integer  "age"
+    t.string   "gender"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
