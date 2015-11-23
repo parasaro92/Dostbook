24018858ee4990f91a245e942ec0f3e8fb387f94
paras <rockypro92@gmail.com>
2015-11-23 11:17:28 +0530

    Added database seeded file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(username: "rockypro92", email: "rockypro92@gmail.com", password: "rocky@lion", password_confirmation: "rocky@lion")
+User.create(username: "rockypro91", email: "rockypro91@gmail.com", password: "rocky@lion", password_confirmation: "rocky@lion")
+User.create(username: "rockypro90", email: "rockypro90@gmail.com", password: "rocky@lion", password_confirmation: "rocky@lion")
+User.create(username: "rockypro89", email: "rockypro89@gmail.com", password: "rocky@lion", password_confirmation: "rocky@lion")
+User.create(username: "rockypro88", email: "rockypro88@gmail.com", password: "rocky@lion", password_confirmation: "rocky@lion")
+p "Test users created"
