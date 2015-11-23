7168556a0a18117ea24b3d34c73ba1695f8b7fe4
paras <rockypro92@gmail.com>
2015-11-23 17:31:22 +0530

    Added friendship status detection

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,4 +25,21 @@ class User < ActiveRecord::Base
   def active_friend
     self.friendships.where(state: "active").map(&:friend) + self.inverse_friendships.where(state: "active").map(&:user)
   end
+
+  def friendship_status(user_2)
+    friendship = Friendship.where(user_id: [self.id, user_2.id], friend_id: [self.id, user_2.id])
+    unless friendship.any?
+      return "not_friends"
+    else
+      if friendship.first.state == "active"
+        return "friends"
+      else
+      if friendship.first.user == self
+        return "pending"
+      else
+        return "requested"
+      end
+      end
+    end
+  end
 end
